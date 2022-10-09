7a480f9bd3fde4622260acee8051a0ae4029835c
Andrea Monterrosa <moralesandrea.edr4@gmail.com>
2022-10-09 17:34:01 +0000

    consulta tiene muchos habitos de consumo y planes alimenticios

--- sirt-api/models/planalimenticio.js
+++ sirt-api/models/planalimenticio.js
@@ -11,6 +11,7 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
+      this.belongsTo(models.Consulta,{as:'Consulta'});
     }
   }
   PlanAlimenticio.init({
