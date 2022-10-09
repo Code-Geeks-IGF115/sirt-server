96e0cfea2fd32c3369eb93f3297415f60f29b019
Andrea Monterrosa <moralesandrea.edr4@gmail.com>
2022-10-09 18:27:19 +0000

    relación ficha tiene muchas consultas

--- sirt-api/models/consulta.js
+++ sirt-api/models/consulta.js
@@ -12,6 +12,7 @@ module.exports = (sequelize, DataTypes) => {
     static associate(models) {
       // define association here
       this.belongsTo(models.Beneficiario,{as:'Beneficiario'});
+      this.belongsTo(models.Ficha,{as:'Ficha'});
     }
   }
   Consulta.init({
