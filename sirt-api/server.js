c665202f03cb5be58702c6b8dc930bd5f646bf08
Nicole Cruz <barrientospamela3@gmail.com>
2022-10-14 17:16:47 -0600

    Creación de la función e inserción de los controladores en el server

--- sirt-api/server.js
+++ sirt-api/server.js
@@ -3,17 +3,22 @@ const express = require("express");
 const app = express();
 require('dotenv').config();
 const bodyParser = require("body-parser");
+const { crearDatosMedicos, verDatosMedicos, manipularDatosM } = require("./controladores/fichaNutricion/DatosMedicos");
 app.use(bodyParser.urlencoded({ extended: false }));
-const {sequelize}=require(__dirname + "/models/index.js");
+const { sequelize } = require(__dirname + "/models/index.js");
 const PORT = process.env.PORT || 3000;
 
 //IMPORTACIONES DE CONTROLADORES
-const {index} = require(__dirname + '/controladores/indexControl.js');
+const { index } = require(__dirname + '/controladores/indexControl.js');
 //MODULO FICHA NUTRICIÓN
-const {crearHistoriaDietetica, 
+const { crearHistoriaDietetica,
   verHistoriasDieteticas,
   verHistoriasDieteticas2,
-  verHistoriasDieteticas3}=require(__dirname + '/controladores/fichaNutricion/HistoriaDieteticaControl.js');
+  verHistoriasDieteticas3 } = require(__dirname + '/controladores/fichaNutricion/HistoriaDieteticaControl.js');
+
+const { crearDatosMedicos,
+  verDatosMedicos,
+  manipularDatosM } = require(__dirname + '/controladores/fichaNutricion/DatosMedicos.js');
 
 
 //RUTAS
@@ -21,24 +26,30 @@ app.route('/').get(index);
 // FICHA NUTRICIÓN
 //Historia Dietética
 app.route('/ficha/nutricion/consulta/historia-dietetica/')
-    .post(crearHistoriaDietetica)
-    .get(verHistoriasDieteticas);
+  .post(crearHistoriaDietetica)
+  .get(verHistoriasDieteticas);
 app.route('/ficha/nutricion/consulta/historia-dietetica2/')
-    .get(verHistoriasDieteticas2);
+  .get(verHistoriasDieteticas2);
 app.route('/ficha/nutricion/consulta/historia-dietetica3/:id')
-    .get(verHistoriasDieteticas3);
-                                                          
+  .get(verHistoriasDieteticas3);
+
+//Datos médicos
+app.route('/ficha/nutricion/consulta/datos/')
+  .post(crearDatosMedicos)
+  .get(verDatosMedicos);
+app.route('/ficha/nutricion/consulta/datos/')
+  .get(manipularDatosM);
 
 
 app.listen(PORT, async function () {
-    try {
-        await sequelize.authenticate();
-        console.log('Connection has been established successfully.');
-        // await sequelize.drop();
-        // console.log("All tables dropped!");
-        // await sequelize.sync();
-      } catch (error) {
-        console.error('Unable to connect to the database:', error);
-      }
-    console.log(`Server is running on Port ${PORT}` );
+  try {
+    await sequelize.authenticate();
+    console.log('Connection has been established successfully.');
+    // await sequelize.drop();
+    // console.log("All tables dropped!");
+    // await sequelize.sync();
+  } catch (error) {
+    console.error('Unable to connect to the database:', error);
+  }
+  console.log(`Server is running on Port ${PORT}`);
 });
