d0b0e48749aaa0f8f5b44e70b8be420c3f9959f1
Andrea Monterrosa <moralesandrea.edr4@gmail.com>
2022-10-16 19:22:24 +0000

    reparacion de crear plan alimenticio y filas del plan alimenticio

--- sirt-api/controladores/fichaNutricion/PlanAlimenticioControl.js
+++ sirt-api/controladores/fichaNutricion/PlanAlimenticioControl.js
@@ -15,70 +15,35 @@ const {PlanAlimenticio,Consulta,FilaPlan,Alimento}=require(`../../models`);
  async function crearPlanAlimenticio(request,response){
     
     let data={'message':"Plan Alimenticio Guardado."}
-    //recuperando el id de la consulta
-    let consultaId=request.body.consultaId;
-    let filas=request.body.filas;
     let hoy = new Date();
+    //recuperando los parametros
+    let {consultaId,filas}=request.body;
     try{
+        
         //recuperando consulta
         const consulta = await Consulta.findOne({ where: { id: parseInt(consultaId)} });
         
         //creando el plan alimenticio y asignando la consulta
-        const planAlimenticio = PlanAlimenticio.create(
+        const planAlimenticio = await PlanAlimenticio.create(
             {
                 "fecha": hoy,
-                "consulta": consulta
-            },
-            {
-                include: [{
-                    association: Consulta,
-                    as: 'consulta'
-                  }]
+                "consultaId": consulta.id
             });
-
-        if(planAlimenticio instanceof PlanAlimenticio){
-            //guardando en la base de datos
-            await planAlimenticio.save();
-        }else{
-            throw 'Datos del plan alimenticio no válidos.';
-        }
-        // //recuperando todos los alimentos de la base de datos
-        // const alimentos=  await Alimento.findAll();
-        // console.log(alimentos[0]);
-        // filas.forEach(async (fila) => {
-        //     //asignando el objeto creado plan alimenticio a la fila plan
-        //     fila.planAlimenticio=planAlimenticio;
-            
-        //     //recuperando alimento
-        //     let alimento=alimentos.filter(alimento => alimento.id == fila.AlimentoId);
-            
-        //     //asignando alimento a la fila plan
-        //     fila.alimento=alimento;
-            
-        //     //creando la fila plan a partir de los datos proporcionados
-        //     const filaPlan = FilaPlan.build(
-        //         fila,
-        //         {
-        //             include: [ PlanAlimenticio ],
-        //             as: 'planAlimenticio'
-        //         },
-        //         {
-        //             include: [ Alimento ],
-        //             as: 'alimento'
-        //         }
-        //         );
-                
-        //         await filaPlan.save();//guardando en la base de datos
-                
-        //     },this);
-            
-        }catch(e){
+    
+        filas.forEach(async (fila) => {
+            //asignando el objeto creado plan alimenticio a la fila plan
+            fila.PlanAlimenticioId=planAlimenticio.id;
+            //creando la fila plan a partir de los datos proporcionados
+            await FilaPlan.create(fila);          
+        },this);
+        
+             
+    }catch(e){
             data={'message':e.message}
-            return response.status(500).json(data);
-        }
+    }
             
-            response.json(data);
-        }
+    return response.json(data);
+}
 
 
 
