b0d0d5caedf1d182d4643e8281a8e6daa3d29927
Andrea Monterrosa <moralesandrea.edr4@gmail.com>
2022-11-04 17:37:26 +0000

    se agrego el archivo consulta control

---
+++ controladores/ConsultaControl.js
@@ -0,0 +1,24 @@
+const { Op } = require("sequelize");
+const { Consulta } = require(`../models`);
+
+/**
+ * Nombre: Pamela Nicole Barrientos Cruz
+ * Carnet: BC21009
+ * Estado: En proceso
+ * Fecha de creación: 4/11/2022
+ * Fecha de última edición: 4/11/2022
+ * Fecha de última revisión:
+ * Fecha de aprobación: 
+ */
+
+//Funcion para ver las consultas
+async function listaConsultasFichaNutricion(request, response){
+    let dato = {}
+    const dui = request.params.dui;
+    //Probando consulta
+    try {
+        
+    } catch (error) {
+        
+    }
+}
